4d6fcd78f4b40b89f7fea30c20163d49bb0c2da7
fjphysics <fjphysics74@gmail.com>
2019-06-10 14:17:19 +0800

    初始化程序

---
+++ src/main.js
@@ -0,0 +1,4 @@
+"use strict";
+var canvas = document.getElementById("canvas");
+var canvas2d = canvas.getContext("2d");
+alert("zzzzzz再次开始");
---
+++ src/main.ts
@@ -0,0 +1,3 @@
+let canvas:HTMLCanvasElement|null=document.getElementById("canvas") as HTMLCanvasElement;
+let canvas2d=canvas.getContext("2d");
+alert("zzzzz开始");
