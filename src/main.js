13ccad3816a11f572c238c04a6bc6b6f93cf7ec8
fjphysics <fjphysics74@gmail.com>
2019-07-09 00:22:20 +0800

    新建文件
    新建文件

--- src/main.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-var canvas = document.getElementById("canvas");
-var canvas2d = canvas.getContext("2d");
-alert("zzzzzz再次开始");
--- src/main.ts
+++
@@ -1,3 +0,0 @@
-let canvas:HTMLCanvasElement|null=document.getElementById("canvas") as HTMLCanvasElement;
-let canvas2d=canvas.getContext("2d");
-alert("zzzzz开始");
